29078037dc5eb25f9156fffcf85d4289290a0948
Priyakshee Mahanta <60322729+Priyaksheemahanta@users.noreply.github.com>
2025-03-10 01:35:12 +0530

    add HomeController

---
+++ src/main/java/com/crud/Learn/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.crud.Learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "Spring Boot is running in Docker!";
+    }
+}
+
